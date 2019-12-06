9c61499b6a29df67ae48c192995e6f659755b864
Oskar Gierszewski <gierszon10@gmail.com>
2019-12-06 18:14:22 +0100

    Basic calculator created.

--- src/App.js
+++ src/App.js
@@ -1,39 +1,77 @@
-import React from 'react';
+import React, { Component } from "react";
 import './App.css';
-import Button from './components/Button';
+import  Button  from './components/Button';
+import { Input } from './components/Input';
+import { render } from '@testing-library/react';
+import * as math from 'mathjs';
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ''
+    };
+  }
+  addToInput = val => {
+    this.setState({ input: this.state.input + val});
+  };
 
-function App() {
-  return (
-    <div className="app">
-      <div className="calc-wrapper">
-        <div className="row">
-          <Button>7</Button>
-          <Button>8</Button>
-          <Button>9</Button>
-          <Button>/</Button>
-        </div>
-        <div className="row">
-          <Button>4</Button>
-          <Button>5</Button>
-          <Button>6</Button>
-          <Button>*</Button>
-        </div>
-        <div className="row">
-          <Button>1</Button>
-          <Button>2</Button>
-          <Button>3</Button>
-          <Button>-</Button>
-        </div>
-        <div className="row">
-          <Button>.</Button>
-          <Button>0</Button>
-          <Button>=</Button>
-          <Button>+</Button>
+  clearInput = val => {
+    this.setState({ input: ""});
+  };
+
+  clearLastInput = () => {
+    console.log(this.state.input)
+    console.log(this.state.input.length)
+    this.setState({ input: this.state.input.toString().substring(0, this.state.input.toString().length - 1)});
+  };
+
+  calculate = () => {
+    var input = "";
+    try {
+      input=math.evaluate(this.state.input);
+    } catch (error) {
+      input="Error";
+    }
+    this.setState({ input: input});
+  };
+
+  render(){
+    return (
+      <div className="app">
+        <div className="calc-wrapper">
+          <Input input={this.state.input}></Input>
+          <div className="row">
+            <Button handleClick={this.addToInput}>7</Button>
+            <Button handleClick={this.addToInput}>8</Button>
+            <Button handleClick={this.addToInput}>9</Button>
+            <Button handleClick={this.addToInput}>/</Button>
+          </div>
+          <div className="row">
+            <Button handleClick={this.addToInput}>4</Button>
+            <Button handleClick={this.addToInput}>5</Button>
+            <Button handleClick={this.addToInput}>6</Button>
+            <Button handleClick={this.addToInput}>*</Button>
+          </div>
+          <div className="row">
+            <Button handleClick={this.addToInput} >1</Button>
+            <Button handleClick={this.addToInput}>2</Button>
+            <Button handleClick={this.addToInput}>3</Button>
+            <Button handleClick={this.addToInput}>-</Button>
+          </div>
+          <div className="row">
+            <Button handleClick={this.addToInput}>.</Button>
+            <Button handleClick={this.addToInput}>0</Button>
+            <Button handleClick={this.calculate}>=</Button>
+            <Button handleClick={this.addToInput}>+</Button>
+          </div>
+          <div className="row">
+            <Button handleClick={this.clearInput}>Clear</Button>
+            <Button handleClick={this.clearLastInput}>CE</Button>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
 export default App;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,16 +1,48 @@
-import React from 'react';
+import React, { Component } from "react";
 import './Button.css';
 import { isNumber } from 'util';
 
-const isFunctionalBtn = val => {
-    return isNaN(val);
+class Button extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        hover: false
+      };
+    }
+
+
+    getButtonFunction = val => {
+        if (!isNaN(val)) {
+            return "standard";
+        }
+        else if(val === 'Clear' || val === 'CE'){
+            return "clear";
+        }
+        else{
+            return "functional";
+        }
+    }
+
+
+    toggleHover = () =>{
+        this.setState ({hover : !this.state.hover});
+        console.log("hover")
+    }
+    isHover = () =>{
+        console.log("isHover")
+        return this.state.hover;
+        
+    }
+    render(){
+        return (
+        <div className={`button ${this.getButtonFunction(this.props.children)} 
+        ${this.isHover() ? "hover" : null }`} 
+        onClick={() => this.props.handleClick(this.props.children)}
+        onMouseEnter={this.toggleHover}
+        onMouseLeave={this.toggleHover}
+        >
+            {this.props.children}
+        </div>)
+    }
 }
-
-const Button = props => (
-    <div className={`button ${isFunctionalBtn(props.children) ? "functional" : "standard"}`}>
-        {props.children}
-    </div>
-)
-
-
 export default Button;
